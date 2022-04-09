4e40ec96b1531ee5143aad76a331d1490d8d8d47
navintc <navin.thamindu@gmail.com>
2022-04-09 22:16:43 +0530

    adding primitive objects

--- Assets/Scripts/girlKeyControl.cs
+++ Assets/Scripts/girlKeyControl.cs
@@ -5,23 +5,5 @@ using UnityEngine;
 public class girlKeyControl : MonoBehaviour
 {
 
-    Rigidbody rb; 
-    public bool movechecker = true;
-    public float speed = 50;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>(); 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (movechecker == true){
-            float x = Input.GetAxisRaw("Horizontal"); 
-            float moveBy = x * speed; 
-            rb.velocity = new Vector2(moveBy, rb.velocity.y); 
-        }
-    }
+  
 }
